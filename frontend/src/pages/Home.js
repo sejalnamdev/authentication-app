071f19cae3cf345688d89c550ec4f7a04d3c7fb3
sejalnamdev <namdevsejal31@gmail.com>
2024-10-06 12:23:31 +0530

    change url

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8080/products";
+            const url = "https://authentication-app-api-beta.vercel.app/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
